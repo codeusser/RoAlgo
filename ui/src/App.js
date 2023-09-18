aa510609ab3031377d901af84b87dfac0b0a0ff1
Vlad Nistor <88168090+codeusser@users.noreply.github.com>
2023-09-18 21:40:22 +0300

    all

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,17 +5,27 @@ import Home from './Home/home';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Member from './BecomeMember/member';
+import Footer from './footer/footer';
+import Anunturi from './Anunturi/anunuri';
+
+
+
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Header></Header>
-      <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path='/member' element={<Member/>}></Route>
+      <div className='content-wrap'> 
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/home" element={<Home/>}></Route>
+            <Route path='/member' element={<Member/>}></Route>
+            <Route path='/anunturi' element={<Anunturi/>}></Route>
         </Routes>
-    </BrowserRouter>
+
+      </BrowserRouter>
+      </div>
+      <Footer/>
     </div>
   );
 }
---
+++ ui/src/footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.scss'
+
+
+export default function Footer(){
+    return (
+        <>
+            <div className='Fcontainer'>
+                <p>© RoAlgo 2023-2023 </p>
+                <p>Site dezvotat cu 🖤 de Nistor Vlad</p>
+            </div>
+        </>
+    )
+}
